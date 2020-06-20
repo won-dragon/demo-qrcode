fe3d30b230a39527b3c0a9e926e77068a639896e
won-dragon <wykim@shi-ict.com>
2020-06-20 23:01:38 +0900

    한글 깨짐 수정

--- app/src/main/java/com/example/myapplication/CreateQR.java
+++ app/src/main/java/com/example/myapplication/CreateQR.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.util.Log;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.net.URLDecoder;
 
 
 public class CreateQR extends AppCompatActivity {
@@ -26,6 +29,12 @@ public class CreateQR extends AppCompatActivity {
         Intent intent = getIntent(); /*데이터 수신*/
 
         String userName = intent.getExtras().getString("userName");
+
+        try{
+            userName = new String(userName.getBytes("UTF-8"), "ISO-8859-1");
+            Log.i("TEST", "onCreate: " + userName);
+        }catch (Exception e){}
+
         String userHp = intent.getExtras().getString("userHp");
         String userEmail = intent.getExtras().getString("userEmail");
 
